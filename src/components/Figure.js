3c9bc93fc56eefb1d5866a9f29448d36a870d497
MartinStoev00 <martinstoev00@gmail.com>
2020-08-15 13:44:38 +0300

    -

--- src/components/Figure.js
+++ src/components/Figure.js
@@ -1,8 +1,6 @@
 import React from 'react'
-import WrongLetters from './WrongLetters'
 
 const Figure = ({wrongLetters}) => {
-    const errors = wrongLetters.length
 
     return (
         <svg height="250" width="200" className="figure-container">
@@ -11,12 +9,12 @@ const Figure = ({wrongLetters}) => {
             <line x1="60" y1="20" x2="60" y2="230" />
             <line x1="20" y1="230" x2="100" y2="230" />
 
-            {errors > 0 && <circle cx="140" cy="70" r="20" />}
-            {errors > 1 && <line x1="140" y1="90" x2="140" y2="150" />}
-            {errors > 2 && <line x1="140" y1="120" x2="120" y2="100" />}
-            {errors > 3 && <line x1="140" y1="120" x2="160" y2="100" />}
-            {errors > 4 && <line x1="140" y1="150" x2="120" y2="180" />}
-            {errors > 5 && <line x1="140" y1="150" x2="160" y2="180" />}
+            {wrongLetters.length > 0 && <circle cx="140" cy="70" r="20" />}
+            {wrongLetters.length > 1 && <line x1="140" y1="90" x2="140" y2="150" />}
+            {wrongLetters.length > 2 && <line x1="140" y1="120" x2="120" y2="100" />}
+            {wrongLetters.length > 3 && <line x1="140" y1="120" x2="160" y2="100" />}
+            {wrongLetters.length > 4 && <line x1="140" y1="150" x2="120" y2="180" />}
+            {wrongLetters.length > 5 && <line x1="140" y1="150" x2="160" y2="180" />}
         </svg>
     )
 }
